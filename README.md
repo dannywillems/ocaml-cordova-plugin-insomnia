2a36784725202f932e1ee4e2ae4bdde94df7b2aa
Danny Willems <contact@danny-willems.be>
2016-03-29 17:18:53 +0200

    update to gen_js_api version

--- README.md
+++ README.md
@@ -14,6 +14,19 @@ Prevent the screen of the mobile device from falling asleep.
 See
 [Insomnia](https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin)
 
+## Repository branches and tags
+
+We are migrating bindings from
+[js_of_ocaml](https://github.com/ocsigen/js_of_ocaml) (low level bindings) to
+[gen_js_api](https://github.com/lexifi/gen_js_api) (high level bindings).
+
+The gen_js_api binding allows to use *pure* ocaml types (you don't have to use
+the ## syntax from js_of_ocaml or Js.string type but only # and string type).
+
+The js_of_ocaml version is available in the branch
+[*js_of_ocaml*](https://github.com/dannywillems/ocaml-cordova-plugin-insomnia/tree/js_of_ocaml)
+but we **recommend** to use the gen_js_api version which is the master branch.
+
 ## How to use ?
 
 See the official documentation
--- insomnia.mli
+++ insomnia.mli
@@ -1,11 +1,33 @@
-type cb_succ
-type cb_err
-
-class type insomnia =
+(* -------------------------------------------------------------------------- *)
+class insomnia : Ojs.t ->
   object
-    method keepAwake : unit Js.meth
-    method allowSleepAgain : unit Js.meth
+    inherit Ojs.obj
+    (* ---------------------------------------------------------------------- *)
+    (* keep_awake *)
+    method keep_awake                 : unit
+    (* keep_awake [success_cb] [error_cb] *)
+    [@@js.call "keepAwake"]
+    method keep_awake_cb              : (unit -> unit) ->
+                                        (unit -> unit) ->
+                                        unit
+    [@@js.call "keepAwake"]
+    (* ---------------------------------------------------------------------- *)
 
-    method keepAwake_cb : cb_succ -> cb_err -> unit Js.meth
-    method allowSleepAgain_cb : cb_succ -> cb_err -> unit Js.meth
+    (* ---------------------------------------------------------------------- *)
+    (* allow_sleep_again *)
+    method allow_sleep_again          : unit
+    [@@js.call "allowSleepAgain"]
+
+    (* allow_sleep_again_cb [success_cb] [error_cb] *)
+    method allow_sleep_again_cb       : (unit -> unit) ->
+                                        (unit -> unit) ->
+                                        unit
+    [@@js.call "allowSleepAgain"]
+    (* ---------------------------------------------------------------------- *)
   end
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+val t : unit -> insomnia
+[@@js.get "window.plugins.insomnia"]
+(* -------------------------------------------------------------------------- *)
