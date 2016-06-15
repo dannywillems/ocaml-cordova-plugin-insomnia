0bcc6f259c2b3ce86a84ec9f714fd2d7cdbeccb0
Danny Willems <contact@danny-willems.be>
2016-06-15 03:10:38 +0200

    Use functions instead of objects

--- cordova_insomnia.mli
+++ cordova_insomnia.mli
@@ -1,33 +1,18 @@
 (* -------------------------------------------------------------------------- *)
-class insomnia : Ojs.t ->
-  object
-    inherit Ojs.obj
-    (* ---------------------------------------------------------------------- *)
-    (* keep_awake *)
-    method keep_awake                 : unit
-    (* keep_awake [success_cb] [error_cb] *)
-    [@@js.call "keepAwake"]
-    method keep_awake_cb              : (unit -> unit) ->
-                                        (unit -> unit) ->
-                                        unit
-    [@@js.call "keepAwake"]
-    (* ---------------------------------------------------------------------- *)
+val keep_awake :
+  ?succ_cb:(unit -> unit)   ->
+  ?error_cb:(unit -> unit)  ->
+  unit                      ->
+  unit
+[@@js.global "window.plugins.insomnia.keepAwake"]
+(* ---------------------------------------------------------------------- *)
 
-    (* ---------------------------------------------------------------------- *)
-    (* allow_sleep_again *)
-    method allow_sleep_again          : unit
-    [@@js.call "allowSleepAgain"]
-
-    (* allow_sleep_again_cb [success_cb] [error_cb] *)
-    method allow_sleep_again_cb       : (unit -> unit) ->
-                                        (unit -> unit) ->
-                                        unit
-    [@@js.call "allowSleepAgain"]
-    (* ---------------------------------------------------------------------- *)
-  end
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> insomnia
-[@@js.get "window.plugins.insomnia"]
-(* -------------------------------------------------------------------------- *)
+(* ---------------------------------------------------------------------- *)
+(* allow_sleep_again_cb ?[success_cb] ?[error_cb] *)
+val allow_sleep_again :
+  ?succ_cb:(unit -> unit)   ->
+  ?error_cb:(unit -> unit)  ->
+  unit                      ->
+  unit
+[@@js.global "window.plugins.insomnia.allowSleepAgain"]
+(* ---------------------------------------------------------------------- *)
